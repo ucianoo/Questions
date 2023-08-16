49e790ec96b0d0f4662a3a029fa60463de850a02
Uchenna <ansonuchenna@gmail.com>
2023-08-16 08:23:12 +0100

    Making questions with react

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import data from './data'
+import Questions from './Questions';
+import {useState} from 'react';
+
+
+const App = () => {
+const [questions, setQuestions] = useState(data);
+  return (
+    <main>
+      <Questions questions={questions} />
+    </main>
+  );
+};
+
+
+export default App;
